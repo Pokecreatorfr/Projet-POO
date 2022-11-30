a4b700852e0300348b60cea1f83cb799feea3f47
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2022-11-30 14:38:13 +0100

    Update MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -52,16 +52,20 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::Panel^ panel3;
 	private: System::Windows::Forms::Panel^ panel7;
 	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ DATE_NAISSANCE_client_tb;
+
 	private: System::Windows::Forms::Panel^ panel6;
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ PRENOMclient_tb;
+
 	private: System::Windows::Forms::Panel^ panel5;
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ NOMclient_Tb;
+
 	private: System::Windows::Forms::Panel^ panel4;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ IDclient_tb;
+
 	private: System::Windows::Forms::Panel^ panel8;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button1;
@@ -72,16 +76,21 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::Button^ button4;
 	private: System::Windows::Forms::Panel^ panel12;
 	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ DATE_NAISSANCE_personnel_tb;
+
 	private: System::Windows::Forms::Panel^ panel13;
 	private: System::Windows::Forms::Label^ label6;
-	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ PRENOMpersonnel_tb;
+
 	private: System::Windows::Forms::Panel^ panel14;
 	private: System::Windows::Forms::Label^ label7;
-	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ NOMpersonnel_tb;
+
 	private: System::Windows::Forms::Panel^ panel15;
 	private: System::Windows::Forms::Label^ label8;
-	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::TextBox^ IDperson_tb;
+
+
 	private: System::Windows::Forms::Panel^ panel16;
 	private: System::Windows::Forms::Label^ Adresse_label;
 	private: System::Windows::Forms::Panel^ panel17;
@@ -131,16 +140,16 @@ namespace ProjetPOO {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->panel7 = (gcnew System::Windows::Forms::Panel());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->DATE_NAISSANCE_client_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel6 = (gcnew System::Windows::Forms::Panel());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->PRENOMclient_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel5 = (gcnew System::Windows::Forms::Panel());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->NOMclient_Tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel4 = (gcnew System::Windows::Forms::Panel());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->IDclient_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel9 = (gcnew System::Windows::Forms::Panel());
 			this->panel10 = (gcnew System::Windows::Forms::Panel());
 			this->panel11 = (gcnew System::Windows::Forms::Panel());
@@ -148,16 +157,16 @@ namespace ProjetPOO {
 			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->panel12 = (gcnew System::Windows::Forms::Panel());
 			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->DATE_NAISSANCE_personnel_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel13 = (gcnew System::Windows::Forms::Panel());
 			this->label6 = (gcnew System::Windows::Forms::Label());
-			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->PRENOMpersonnel_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel14 = (gcnew System::Windows::Forms::Panel());
 			this->label7 = (gcnew System::Windows::Forms::Label());
-			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->NOMpersonnel_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel15 = (gcnew System::Windows::Forms::Panel());
 			this->label8 = (gcnew System::Windows::Forms::Label());
-			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->IDperson_tb = (gcnew System::Windows::Forms::TextBox());
 			this->panel16 = (gcnew System::Windows::Forms::Panel());
 			this->Adresse_label = (gcnew System::Windows::Forms::Label());
 			this->panel17 = (gcnew System::Windows::Forms::Panel());
@@ -337,7 +346,7 @@ namespace ProjetPOO {
 			// panel7
 			// 
 			this->panel7->Controls->Add(this->label4);
-			this->panel7->Controls->Add(this->textBox4);
+			this->panel7->Controls->Add(this->DATE_NAISSANCE_client_tb);
 			this->panel7->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel7->Location = System::Drawing::Point(0, 174);
 			this->panel7->Name = L"panel7";
@@ -357,18 +366,18 @@ namespace ProjetPOO {
 			this->label4->TabIndex = 1;
 			this->label4->Text = L"Date de naissance";
 			// 
-			// textBox4
+			// DATE_NAISSANCE_client_tb
 			// 
-			this->textBox4->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox4->Location = System::Drawing::Point(0, 32);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(314, 22);
-			this->textBox4->TabIndex = 0;
+			this->DATE_NAISSANCE_client_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->DATE_NAISSANCE_client_tb->Location = System::Drawing::Point(0, 32);
+			this->DATE_NAISSANCE_client_tb->Name = L"DATE_NAISSANCE_client_tb";
+			this->DATE_NAISSANCE_client_tb->Size = System::Drawing::Size(314, 22);
+			this->DATE_NAISSANCE_client_tb->TabIndex = 0;
 			// 
 			// panel6
 			// 
 			this->panel6->Controls->Add(this->label3);
-			this->panel6->Controls->Add(this->textBox3);
+			this->panel6->Controls->Add(this->PRENOMclient_tb);
 			this->panel6->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel6->Location = System::Drawing::Point(0, 116);
 			this->panel6->Name = L"panel6";
@@ -388,18 +397,18 @@ namespace ProjetPOO {
 			this->label3->TabIndex = 1;
 			this->label3->Text = L"Prénom";
 			// 
-			// textBox3
+			// PRENOMclient_tb
 			// 
-			this->textBox3->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox3->Location = System::Drawing::Point(0, 36);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(314, 22);
-			this->textBox3->TabIndex = 0;
+			this->PRENOMclient_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->PRENOMclient_tb->Location = System::Drawing::Point(0, 36);
+			this->PRENOMclient_tb->Name = L"PRENOMclient_tb";
+			this->PRENOMclient_tb->Size = System::Drawing::Size(314, 22);
+			this->PRENOMclient_tb->TabIndex = 0;
 			// 
 			// panel5
 			// 
 			this->panel5->Controls->Add(this->label2);
-			this->panel5->Controls->Add(this->textBox2);
+			this->panel5->Controls->Add(this->NOMclient_Tb);
 			this->panel5->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel5->Location = System::Drawing::Point(0, 58);
 			this->panel5->Name = L"panel5";
@@ -419,18 +428,18 @@ namespace ProjetPOO {
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Nom";
 			// 
-			// textBox2
+			// NOMclient_Tb
 			// 
-			this->textBox2->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox2->Location = System::Drawing::Point(0, 36);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(314, 22);
-			this->textBox2->TabIndex = 0;
+			this->NOMclient_Tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->NOMclient_Tb->Location = System::Drawing::Point(0, 36);
+			this->NOMclient_Tb->Name = L"NOMclient_Tb";
+			this->NOMclient_Tb->Size = System::Drawing::Size(314, 22);
+			this->NOMclient_Tb->TabIndex = 0;
 			// 
 			// panel4
 			// 
 			this->panel4->Controls->Add(this->label1);
-			this->panel4->Controls->Add(this->textBox1);
+			this->panel4->Controls->Add(this->IDclient_tb);
 			this->panel4->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel4->Location = System::Drawing::Point(0, 0);
 			this->panel4->Name = L"panel4";
@@ -450,13 +459,13 @@ namespace ProjetPOO {
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"ID";
 			// 
-			// textBox1
+			// IDclient_tb
 			// 
-			this->textBox1->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox1->Location = System::Drawing::Point(0, 36);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(314, 22);
-			this->textBox1->TabIndex = 0;
+			this->IDclient_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->IDclient_tb->Location = System::Drawing::Point(0, 36);
+			this->IDclient_tb->Name = L"IDclient_tb";
+			this->IDclient_tb->Size = System::Drawing::Size(314, 22);
+			this->IDclient_tb->TabIndex = 0;
 			// 
 			// panel9
 			// 
@@ -520,7 +529,7 @@ namespace ProjetPOO {
 			// panel12
 			// 
 			this->panel12->Controls->Add(this->label5);
-			this->panel12->Controls->Add(this->textBox5);
+			this->panel12->Controls->Add(this->DATE_NAISSANCE_personnel_tb);
 			this->panel12->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel12->Location = System::Drawing::Point(0, 174);
 			this->panel12->Name = L"panel12";
@@ -540,18 +549,18 @@ namespace ProjetPOO {
 			this->label5->TabIndex = 1;
 			this->label5->Text = L"Date de naissance";
 			// 
-			// textBox5
+			// DATE_NAISSANCE_personnel_tb
 			// 
-			this->textBox5->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox5->Location = System::Drawing::Point(0, 32);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(314, 22);
-			this->textBox5->TabIndex = 0;
+			this->DATE_NAISSANCE_personnel_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->DATE_NAISSANCE_personnel_tb->Location = System::Drawing::Point(0, 32);
+			this->DATE_NAISSANCE_personnel_tb->Name = L"DATE_NAISSANCE_personnel_tb";
+			this->DATE_NAISSANCE_personnel_tb->Size = System::Drawing::Size(314, 22);
+			this->DATE_NAISSANCE_personnel_tb->TabIndex = 0;
 			// 
 			// panel13
 			// 
 			this->panel13->Controls->Add(this->label6);
-			this->panel13->Controls->Add(this->textBox6);
+			this->panel13->Controls->Add(this->PRENOMpersonnel_tb);
 			this->panel13->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel13->Location = System::Drawing::Point(0, 116);
 			this->panel13->Name = L"panel13";
@@ -571,18 +580,18 @@ namespace ProjetPOO {
 			this->label6->TabIndex = 1;
 			this->label6->Text = L"Prénom";
 			// 
-			// textBox6
+			// PRENOMpersonnel_tb
 			// 
-			this->textBox6->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox6->Location = System::Drawing::Point(0, 36);
-			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(314, 22);
-			this->textBox6->TabIndex = 0;
+			this->PRENOMpersonnel_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->PRENOMpersonnel_tb->Location = System::Drawing::Point(0, 36);
+			this->PRENOMpersonnel_tb->Name = L"PRENOMpersonnel_tb";
+			this->PRENOMpersonnel_tb->Size = System::Drawing::Size(314, 22);
+			this->PRENOMpersonnel_tb->TabIndex = 0;
 			// 
 			// panel14
 			// 
 			this->panel14->Controls->Add(this->label7);
-			this->panel14->Controls->Add(this->textBox7);
+			this->panel14->Controls->Add(this->NOMpersonnel_tb);
 			this->panel14->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel14->Location = System::Drawing::Point(0, 58);
 			this->panel14->Name = L"panel14";
@@ -602,18 +611,18 @@ namespace ProjetPOO {
 			this->label7->TabIndex = 1;
 			this->label7->Text = L"Nom";
 			// 
-			// textBox7
+			// NOMpersonnel_tb
 			// 
-			this->textBox7->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox7->Location = System::Drawing::Point(0, 36);
-			this->textBox7->Name = L"textBox7";
-			this->textBox7->Size = System::Drawing::Size(314, 22);
-			this->textBox7->TabIndex = 0;
+			this->NOMpersonnel_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->NOMpersonnel_tb->Location = System::Drawing::Point(0, 36);
+			this->NOMpersonnel_tb->Name = L"NOMpersonnel_tb";
+			this->NOMpersonnel_tb->Size = System::Drawing::Size(314, 22);
+			this->NOMpersonnel_tb->TabIndex = 0;
 			// 
 			// panel15
 			// 
 			this->panel15->Controls->Add(this->label8);
-			this->panel15->Controls->Add(this->textBox8);
+			this->panel15->Controls->Add(this->IDperson_tb);
 			this->panel15->Dock = System::Windows::Forms::DockStyle::Top;
 			this->panel15->Location = System::Drawing::Point(0, 0);
 			this->panel15->Name = L"panel15";
@@ -634,13 +643,13 @@ namespace ProjetPOO {
 			this->label8->Text = L"ID2";
 			this->label8->Click += gcnew System::EventHandler(this, &MyForm::label8_Click);
 			// 
-			// textBox8
+			// IDperson_tb
 			// 
-			this->textBox8->Dock = System::Windows::Forms::DockStyle::Bottom;
-			this->textBox8->Location = System::Drawing::Point(0, 36);
-			this->textBox8->Name = L"textBox8";
-			this->textBox8->Size = System::Drawing::Size(314, 22);
-			this->textBox8->TabIndex = 0;
+			this->IDperson_tb->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->IDperson_tb->Location = System::Drawing::Point(0, 36);
+			this->IDperson_tb->Name = L"IDperson_tb";
+			this->IDperson_tb->Size = System::Drawing::Size(314, 22);
+			this->IDperson_tb->TabIndex = 0;
 			// 
 			// panel16
 			// 
