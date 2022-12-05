821056e191943f2d32ad610cd41ed534d288a8a3
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2022-12-05 22:04:27 +0100

    Connexion a la BDD depuis le fichier config

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,17 @@
 #include "MyForm.h"
+#include "DatabaseAccess.h"
 using namespace System;
 using namespace System::Windows::Forms;
 using namespace System::IO;
+using namespace System::Configuration;
 
 
 [STAThreadAttribute]
 int __clrcall WinMain(array<String^>^ args)
 {
+	String^ abc = System::Configuration::ConfigurationManager::AppSettings["Data Source"];
+	Composants::DatabaseAccess^ db = gcnew Composants::DatabaseAccess(System::Configuration::ConfigurationManager::AppSettings["Data Source"], System::Configuration::ConfigurationManager::AppSettings["Initial Catalog"], System::Configuration::ConfigurationManager::AppSettings["User ID"], System::Configuration::ConfigurationManager::AppSettings["Password"]);
+	
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	ProjetPOO::MyForm monFormulaire;
