f94811a642804ea4982755505df14413006d89ee
Ysterly14 <116062836+Ysterly14@users.noreply.github.com>
2022-12-07 14:14:46 +0100

    Bons PRODUITS

--- ProduitDAO.cpp
+++ ProduitDAO.cpp
@@ -1,1 +1,81 @@
 #include "ProduitDAO.h"
+
+String^ ProduitDAO::Delete(String^ id)
+{
+    String^ query = "DELETE FROM dbo.Produit";
+    query += " WHERE ID_pr = " + id + "";
+    return query;
+}
+
+String^ ProduitDAO::Search(String^ id, String^ Ref_pr, String^ Designation_pr, String^ Prix_HT_actuel_pr, String^ TVA_actuelle_pr, String^ qte_pr, String^ SRea_pr)
+{
+	int a = 0;
+	String^ query = "SELECT * FROM dbo.Produit WHERE ";
+	if (Ref_pr != "")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "Ref_pr = '" + Ref_pr + "'";
+		a++;
+	}
+	if (Designation_pr != "")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "Designation_pr = '" + Designation_pr + "'";
+		a++;
+	}
+	if (Prix_HT_actuel_pr != "  /  /")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "Prix_HT_actuel_pr = '" + Prix_HT_actuel_pr + "'  ";
+		a++;
+	}
+	if (TVA_actuelle_pr != "  /  /")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "TVA_actuelle_pr = '" + TVA_actuelle_pr + "'  ";
+		a++;
+	}
+	if (qte_pr != "NULL")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "qte_pr = '" + qte_pr + "'  ";
+		a++;
+	}
+	if (SRea_pr != "")
+	{
+		if (a > 0)
+		{
+			query += " AND ";
+		}
+		query += "SRea_pr = '" + SRea_pr + "'  ";
+		a++;
+	}
+	return query;
+}
+
+String^ ProduitDAO::Insert(String^ id, String^ Ref_pr, String^ Designation_pr, String^ Prix_HT_actuel_pr, String^ TVA_actuelle_pr, String^ qte_pr, String^ SRea_pr)
+{
+	String^ query = "INSERT INTO dbo.Produit (Ref_pr, Designation_pr , Prix_HT_actuel_pr , TVA_actuelle_pr , qte_pr , SREA_pr) VALUES ('" + Ref_pr + "', " + Designation_pr + ", " + Prix_HT_actuel_pr + " , '" + TVA_actuelle_pr + "', " + qte_pr + ", " + SRea_pr + ")";
+	return query;
+}
+
+String^ ProduitDAO::Update(String^ id, String^ Ref_pr, String^ Designation_pr, String^ Prix_HT_actuel_pr, String^ TVA_actuelle_pr, String^ qte_pr, String^ SRea_pr)
+{
+	String^ query = "UPDATE dbo.Produit SET Ref_pr = '" + Ref_pr + "', Designation_pr = " + Designation_pr + ", Prix_HT_actuel_pr = " + Prix_HT_actuel_pr + ", TVA_actuelle_pr = '" + TVA_actuelle_pr + "'qte_pr = '" + qte_pr + " 'SRea_pr ='" + SRea_pr + " WHERE ID_pl = " + id + "";
+	return query;
+}
