89682b47595c6dff0332dd2115c5d2abdb8352f4
Dragonkell <113989412+Dragonkell@users.noreply.github.com>
2022-12-07 10:47:50 +0100

    Création Test Unitaire + changement NomAcces Setting

---
+++ UnitTestPOO/UnitTestPOO/UnitTestPOO.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestPOO
+{
+	TEST_CLASS(UnitTestPOO)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
