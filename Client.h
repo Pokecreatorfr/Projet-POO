91f348ebb64703a6dcdee94b8ad5085068adb8b8
Ysterly14 <116062836+Ysterly14@users.noreply.github.com>
2022-12-07 01:01:37 +0100

    Clients Bons

--- Client.h
+++ Client.h
@@ -1,21 +1,14 @@
 #pragma once
 #include<string>
-#include"Personne.h"
+#include "Personne.h"
+#include "PersonneDAO.h"
+#include "ClientDAO.h"
+#include "DatabaseAccess.h"
 using namespace std;
 
 ref class Client : public Composants::MappingPERSONNE
-
-{private:
-	int ID_cl;
-	DateTime Date;
-
- public:
-	 Client();
-	 Client(int ID_cl,String^ Nom,String^ Prenom,DateTime DateNaissance,DateTime Date);
-	 int get_ID_cl();
-	 void set_ID_cl();
-	 void set_Date();
-	 DateTime get_Date();
-
+{
+	static DataSet^ Search(Composants::DatabaseAccess^ bdd, String^ id, String^ nom, String^ prenom, String^ DateNaissance, String^ Date);
+	static void Insert(Composants::DatabaseAccess^ bdd, String^ id, String^ nom, String^ prenom, String^ DateNaissance, String^ Date);
 };
 
