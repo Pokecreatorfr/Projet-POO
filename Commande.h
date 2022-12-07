2b6442656ce347ed9c27fa06efb5fc67ee5a3751
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2022-12-07 06:00:40 +0100

    Update Commande.h

--- Commande.h
+++ Commande.h
@@ -10,4 +10,5 @@ public:
 	static DataSet^ Search(Composants::DatabaseAccess^ bdd, String^ id, String^ date_c, String^ date_l, String^ id_cl, String^ ref);
 	static void Insert(Composants::DatabaseAccess^ bdd, String^ date_c, String^ date_l, String^ id_cl, String^ ref);
 	static void Delete(Composants::DatabaseAccess^ bdd, String^ id);
+	static void Update(Composants::DatabaseAccess^ bdd, String^ date_c, String^ date_l, String^ id_cl, String^ ref, String^ id);
 };
